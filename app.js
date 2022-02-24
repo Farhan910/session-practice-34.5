208b84c6005c20674af5bc3c4c428e30f882da79
Farhan910 <farhanfaruk221@gmail.comm>
2022-02-24 18:42:22 +0600

    3rd

--- app.js
+++ app.js
@@ -7,9 +7,17 @@ const loadDog = () =>{
 const outDog = (dogList) =>{
 
     const dogOut = document.getElementById('dog-out')
-    const first10Dog = dogList.slice(0,10)
-    const div = document.createElement('div')
+    const first10Dog = dogList.slice(0,100)
+    
     for(const dog of first10Dog){
-       
+       const div = document.createElement('div')
+       div.className ="col-lg-4"
+       console.log(dog);
+       div.innerHTML = `
+       <h2>${dog.name}</h2>
+       <p> height  ${dog.height.metric}</p>
+       <img width="400px" height="300px" src="${dog.image.url}"/>
+       `
+      dogOut.appendChild(div)
     }
 }
