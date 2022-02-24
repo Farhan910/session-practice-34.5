64a0c97fea849f39ac0435ae7170da00534e9931
Farhan910 <farhanfaruk221@gmail.comm>
2022-02-24 21:14:13 +0600

    6th

--- cards/card.js
+++ cards/card.js
@@ -1,3 +1,28 @@
 const searchButton = () =>{
-    console.log('hey');
+    const inputValue = document.getElementById('input-value')
+    const error = document.getElementById('error')
+    const searchInput = parseInt(inputValue.value)
+
+    if(isNaN(searchInput) || searchInput == ''){
+        error.innerText = 'Please give a number'
+        inputValue.value = '';
+    }
+    else if(searchInput <= 0 ){
+        error.innerText = 'Please give a positive number'
+        inputValue.value = '';
+    }
+    else if (searchInput > 52){
+        error.innerText = "Please give a number what isn't bigger then 52 "
+    }
+    else{
+    fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=${searchInput}`)
+    .then(res => res.json())
+    .then(data => cardDisplay(data))
+         inputValue.value = '';
+    }
+        
+}
+
+const cardDisplay = (cards) =>{
+    console.log(cards)
 }
